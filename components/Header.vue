1680a641dd969b216dd6917630af0acd5ce5aff9
mostafa rahmani <mostafaa_rahmani@outlook.com>
2023-11-15 19:48:24 +0330

    base ui layers and structure

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <UCard>
+    <div class="flex justify-between">
+      <div class="mr-auto flex">
+        <div class="mr-2 my-auto">Github</div>
+      </div>
+      <div class="ml-auto flex">
+        <UButton
+          :icon="
+            isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+          "
+          color="gray"
+          variant="ghost"
+          class="my-auto"
+          aria-label="Theme"
+          @click="isDark = !isDark"
+        />
+      </div>
+    </div>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <UContainer class="my-5">
+    <Header class="mb-3" />
+    <div class="flex flex-col lg:flex-row gap-3">
+      <UCard class="w-full lg:w-1/2">
+        <template v-slot:header> Your test configurations</template>
+        <UTextarea :rows="20" v-model="value" />
+        <template v-slot:footer> Token count: 321</template>
+      </UCard>
+      <UCard class="w-full lg:w-1/2">
+        <template #header> Generated Tests </template>
+        <VCodeBlock
+          :code="code"
+          highlightjs
+          lang="javascript"
+          :theme="isDark ? 'ir-black' : 'grayscale'"
+        />
+      </UCard>
+    </div>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import VCodeBlock from "@wdns/vue-code-block";
+const currentTheme = useColorMode();
+const isDark = computed(() => {
+  return currentTheme.value === "dark";
+});
+const value = ref("");
+
+const code = ref(`<script>const foo = 'bar';<script>`);
+</script>
+
+<style scoped></style>
